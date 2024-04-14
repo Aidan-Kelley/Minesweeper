ea9a397324eac7f9280b1acbcc208b0cf7ea6c8f
Aidan-Kelley~ <aidank6@yahoo.com>
2024-04-14 01:07:00 -0700

    input error handling

--- Minesweeper.java
+++ Minesweeper.java
@@ -35,35 +35,41 @@ public class Minesweeper {
     }
 
     private int[] getUserAction() {
-        System.out.print("Enter: ");
-        String ans = scan.nextLine();
-        ans = ans.toUpperCase();
-        String x = ans.substring(ans.indexOf(" ") + 1, ans.indexOf(" ", ans.indexOf(" ") + 1));
-        String y = ans.substring(1 + ans.indexOf(" ", ans.indexOf(" ") + 1));
         int[] action = new int[3];
-
-        if (ans.substring(0, 1).equals("F")) {// if flag
-            action[0] = 1;
-        } else if (ans.substring(0, 1).equals("B")) {// if break
-            action[0] = 2;
-        } else {// assume unflag
-            action[0] = 3;
-        }
-
-        // check for coordinates
-        for (int i = 1; i <= COLUMNS; i++) {// extracts column value
-            if (x.equals(i + "")) {
-                action[1] = i;
-                break;
+        String choice; // want to rename the choice variable, it stores the F, B, or U
+        int x, y;
+        while (true) {
+            System.out.print("Enter: ");
+            choice = scan.next().toUpperCase();
+            if (choice.equals("F")) {// if flag
+                action[0] = 1;
+            } else if (choice.equals("B")) {// if break
+                action[0] = 2;
+            } else if (choice.equals("U")) {// if unflag
+                action[0] = 3;
+            } else {
+                System.out.println("Start your action with F, B, or U");
+                scan.nextLine();
+                continue;
             }
-        }
-        for (int i = 1; i <= ROWS; i++) {// extracts row value
-            if (y.equals("" + i)) {
-                action[2] = i;
-                break;
+            try {
+                x = scan.nextInt();
+                y = scan.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid Input");
+                scan.nextLine();
+                continue;
             }
+
+            if (x < 1 || x > COLUMNS || y < 1 || y > ROWS) {
+                System.out.println("Invalid coordinate");
+                scan.nextLine();
+                continue;
+            }
+            action[1] = x;
+            action[2] = y;
+            return action;
         }
-        return action;
     }
 
     private void end(boolean won) {
@@ -73,25 +79,5 @@ public class Minesweeper {
         } else {
             Graphics.displayLoseMessage();
         }
-    }
-
-    private void showTrueField(int[][] field) {
-        System.out.print("  ");
-        for (int i = 1; i <= field[0].length; i++) { // add the column numbers
-            System.out.print(" " + i + " ");
-        }
-        System.out.println();
-        for (int r = 0; r < field.length; r++) { // add the row numbers
-            System.out.print((r + 1) + " ");
-            if (r < 9) {
-                System.out.print(" ");
-            }
-            for (int c = 0; c < field[0].length; c++) {
-                System.out.print(field[r][c] + ", ");
-            }
-            System.out.println();
-        }
-        System.out.println("\nF = Flagged");
-
     }
 }
